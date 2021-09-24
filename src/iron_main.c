65508fbacea9f6a7aadd4321b55bc6ffccc1d7bb
POKA <pokareborn@gmail.com>
2021-09-24 23:35:03 +0800

    aabb test

--- src/iron_main.c
+++ src/iron_main.c
@@ -8,6 +8,7 @@
 typedef struct {
     V2f pos;
     V2f speed;
+    V2f size;
     Color c;
 } Entity;
 
@@ -35,46 +36,70 @@ int main() {
         return load_tex_res;
     }
 
-    Entity e;
-    e.pos.x = 0.0f;
-    e.pos.y = 300.0f;
-    e.speed.x = 150.0f;
-    e.speed.y = 0.0f;
-    e.c = COLOR_PURPLE;
-    
-    V2f size = {
-        25.0f,
-        25.0f
-    };
+    Entity box;
+    box.pos.x = 400.0f;
+    box.pos.y = 300.0f;
+    box.speed.x = 0.0f;
+    box.speed.y = 0.0f;
+    box.c = COLOR_WHITE;
+    box.size.x = 200.0f;
+    box.size.y = 200.0f;
+
+    Entity p;
+    p.pos.x = 200.0f;
+    p.pos.y = 200.0f;
+    p.speed.x = 300.0f;
+    p.speed.y = 300.0f;
+    p.c = COLOR_WHITE;
+    p.size.x = 75.0f;
+    p.size.y = 75.0f;
 
     V2f wnd_sz =  GetWindowWH();
 
-    SetTargetFPS(10);
-    
-    float current_time = GetTime();
-    float previous_time = GetTime();
-    float time_elapsed = 0.0f;
-
-    int test_count = 0;
+    SetTargetFPS(60);
 
     // Game loop
     while (IsWindowRunning()) {
-        
-        if (e.pos.x > 800.0f) {
-            current_time = GetTime();
-            time_elapsed = current_time - previous_time;
-            previous_time = current_time;
 
-            LogInfo("Time elapsed: %f", time_elapsed);
-            e.pos.x = 0.0f;
+        float dt = GetDeltaTime();
+        if (IsKeyDown(KEY_RIGHT)) {
+            p.pos.x += p.speed.x * dt;
+        } else if (IsKeyDown(KEY_LEFT)) {
+            p.pos.x -= p.speed.x * dt;
         }
 
-        e.pos = V2fAdd(e.pos, V2fScalef(e.speed, GetDeltaTime()));
+        if (IsKeyDown(KEY_DOWN)) {
+            p.pos.y += p.speed.y * dt;
+        } else if (IsKeyDown(KEY_UP)) {
+            p.pos.y -= p.speed.y * dt;
+        }
+
+        // AABB
+        float box_x_min = box.pos.x - box.size.x / 2.0f;
+        float box_x_max = box.pos.x + box.size.x / 2.0f;
+        float box_y_min = box.pos.y - box.size.y / 2.0f;
+        float box_y_max = box.pos.y + box.size.y / 2.0f;
+
+        float p_x_min = p.pos.x - p.size.x / 2.0f;
+        float p_x_max = p.pos.x + p.size.x / 2.0f;
+        float p_y_min = p.pos.y - p.size.y / 2.0f;
+        float p_y_max = p.pos.y + p.size.y / 2.0f;
+
+        if (p_x_max >= box_x_min && p_x_min <= box_x_max
+            && p_y_max >= box_y_min && p_y_min <= box_y_max) {
+            p.c = COLOR_PURPLE;
+            box.c = COLOR_ORANGE;
+        } else {
+            p.c = COLOR_WHITE;
+            box.c = COLOR_WHITE;
+        }
 
         StartScene(COLOR_BLACK);
         BeginRendering();
 
-        DrawRectangle(e.pos, size, 0.0f, e.c);
+        DrawRectangle(box.pos, box.size, 0.0f, box.c);
+
+        DrawRectangle(p.pos, p.size, 0.0f, p.c);
 
         EndRendering();
         EndScene();
